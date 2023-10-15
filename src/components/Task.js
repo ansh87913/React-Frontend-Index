bf37d0796d964fea0e26cf9abf3cc40f26d667de
ansh87913 <96718331+ansh87913@users.noreply.github.com>
2023-10-15 17:37:08 +0530

    Update Task.js

--- src/components/Task.js
+++ src/components/Task.js
@@ -5,38 +5,49 @@ export function Task()
     return(
         <div class="table">
             <table>
+                <thead>
+                    <th>Task NO.</th>
+                    <th>Github Link</th>
+                    <th>Deployment Link</th>
+                </thead>
                 <tr>
                     <td>Task1</td>
-                    <td><a href="https://react-frontend-1-qz4882l5c-ansh87913s-projects.vercel.app/">Task 1</a></td>
+                    <td><a href="https://github.com/ansh87913/React-Frontend-1" target="_main">Link 1</a></td>
+                    <td><a href="https://react-frontend-1.vercel.app/" target="_main">Link 1</a></td>
                 </tr>
                 <tr>
                     <td>Task2</td>
+                    <td><a href="https://github.com/ansh87913/React-Frontend-2" target="_main">Link 1</a></td>
                     <td>
-                        <a href="https://react-frontend-1-8k34l6ome-ansh87913s-projects.vercel.app/">Link 2</a>
+                        <a href="https://react-frontend-2.vercel.app/" target="_main">Link 2</a>
                     </td>
                 </tr>
                 <tr>
                     <td>Task 3</td>
+                    <td><a href="https://github.com/ansh87913/React-Frontend-3" target="_main">Link 1</a></td>
                     <td>
-                        <a href="https://react-frontend-1-rk6tvnzvv-ansh87913s-projects.vercel.app/">Task 3</a>
+                        <a href="https://react-frontend-3.vercel.app/" target="_main">Link 3</a>
                     </td>
                 </tr>
                 <tr>
                     <td>Task 4</td>
+                    <td><a href="https://github.com/ansh87913/React-Frontend-4" target="_main">Link 1</a></td>
                     <td>
-                        <a href="https://react-frontend-1-80k00foxh-ansh87913s-projects.vercel.app/">Link 4</a>
+                        <a href="https://react-frontend-4.vercel.app/" target="_main">Link 4</a>
                     </td>
                 </tr>
                 <tr>
                     <td>Task 5</td>
+                    <td><a href="https://github.com/ansh87913/React-Frontend-5" target="_main">Link 1</a></td>
                     <td>
-                        <a href="https://react-frontend-1-8azny7v0r-ansh87913s-projects.vercel.app/">Link 5</a>
+                        <a href="https://react-frontend-5.vercel.app/" target="_main">Link 5</a>
                     </td>
                 </tr>
                 <tr>
                     <td>Task 6</td>
+                    <td><a href="https://github.com/ansh87913/React-Frontend-6" target="_main">Link 1</a></td>
                     <td>
-                        <a href="https://react-frontend-1-qz4882l5c-ansh87913s-projects.vercel.app/">Link 6</a>
+                        <a href="https://react-frontend-6.vercel.app/" target="_main">Link 6</a>
                     </td>
                 </tr>
             </table>
